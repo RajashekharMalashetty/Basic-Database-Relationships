d18b4ba786a0b63a7655be89219c84226f32054d
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-04-24 13:55:01 +0530

    creating posts Schema

---
+++ Models/posts.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main().then(() => console.log("connection successful")).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const userSchema = new Schema({
+    username: String,
+    email: String,
+});
+
+const postSchema = new Schema({
+    content: String,
+    likes: Number,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
+});
+
